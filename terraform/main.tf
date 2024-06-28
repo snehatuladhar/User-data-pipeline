2ca1e944251a9dbf8db52a53742604bc442a7325
snehatuladhar <snehatuladhar098@gmail.com>
2024-06-28 10:41:19 +0545

    initial commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -50,12 +50,3 @@ resource "aws_s3_bucket" "sneha-bucket" {
 }
 
 # resource "aws_s3_bucket_object" "sneha-bucket" {
-#   bucket = "my-tf-test-bucket"
-#   key    = "new_object_key"
-#   source = "path/to/file"
-
-#   # The filemd5() function is available in Terraform 0.11.12 and later
-#   # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
-#   # etag = "${md5(file("path/to/file"))}"
-#   etag = filemd5("path/to/file")
-# }
